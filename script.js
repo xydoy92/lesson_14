dc12e57a0d97603809899b10a98aff8d21256770
xydoy92 <xydoy92@gmail.com>
2020-07-21 14:38:56 +0300

    fix: naming fixed

--- script.js
+++ script.js
@@ -6,27 +6,27 @@ let persons = [
 	{ id: 13, name: 'Zoya', age: 9, parentId: 12 },
 	{ id: 14, name: 'Arkadiy', age: 45 },
 ];
-let adult = [];
-let notAdult = [];
+let adultList = [];
+let childList = [];
 
 for (let i = 0; i < persons.length; i++) {
-	if (persons[i]["age"] >= 18) {
-		adult.push(persons[i]);
+	if (persons[i].age >= 18) {
+		adultList.push(persons[i]);
 		// Узнаём профессию
-		persons[i]["job"] = prompt(`Кем вы работаете, ${persons[i]["name"]}?`);
+		persons[i].job = prompt(`Кем вы работаете, ${persons[i].name}?`);
 	} else {
-		notAdult.push(persons[i]);
+		childList.push(persons[i]);
 	}
 }
 
-for (let i = 0; i < adult.length; i++) {
-	for (let j = 0; j < notAdult.length; j++) {
-		if (notAdult[j]["parentId"] === adult[i]["id"]) {
-			adult[i]["childId"] = notAdult[j]["id"];
+for (let i = 0; i < adultList.length; i++) {
+	for (let j = 0; j < childList.length; j++) {
+		if (childList[j].parentId === adultList[i].id) {
+			adultList[i].childId = childList[j].id;
 		}
 	}
 }
 	// Проверка, что всё сработало
 console.log(persons);
-console.log(adult);
-console.log(notAdult);
+console.log(adultList);
+console.log(childList);
